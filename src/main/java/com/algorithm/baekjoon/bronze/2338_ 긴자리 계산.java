49e85e89b4fe2d8c12c4984efec7ff21d320012f
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-01-23 13:25:15 +0900

    Finished `#2338_ 긴자리 계산`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/2338_ 긴자리 계산.java
@@ -0,0 +1,27 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        BigInteger firstInputLine = new BigInteger(br.readLine());
+        BigInteger secondInputLine = new BigInteger(br.readLine());
+
+        String firstOutputLine = String.valueOf( firstInputLine.add(secondInputLine) );
+        String secondOutputLine = String.valueOf( firstInputLine.subtract(secondInputLine) );
+        String thirdOutputLine = String.valueOf( firstInputLine.multiply(secondInputLine) );
+
+        bw.write(firstOutputLine);
+        bw.newLine();
+        bw.write(secondOutputLine);
+        bw.newLine();
+        bw.write(thirdOutputLine);
+        bw.flush();
+        bw.close();
+    }
+}
