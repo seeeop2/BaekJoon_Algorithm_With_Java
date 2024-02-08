4ffb0794a3c4d68ad65863543eac2e8e61e48f2b
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-02-08 11:07:46 +0900

    Finished `#5338_마이크로소프트 로고`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/5338_마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        bw.write("       _.-;;-._\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-;;-._|\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-''-._|");
+
+        bw.flush();
+        bw.close();
+    }
+}
