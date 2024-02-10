f28e16856e87fded884987257db170f365ac30ed
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-02-10 17:26:49 +0900

    Finished `#5341_Pyramids`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/5341_Pyramids.java
@@ -0,0 +1,26 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int input = 0;
+
+        while ( (input =  Integer.parseInt(br.readLine())) != 0 ) {
+
+            int needNumberOfBlocks = 0;
+
+            for (int i = 1 ; i <= input ; i++) {
+                needNumberOfBlocks += i;
+            }
+            bw.write(String.valueOf( needNumberOfBlocks ));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
