cbac112f5c893c2901ada8d119764525785e0c1a
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-02-17 11:48:39 +0900

    Finished `#8545_Zadanie próbne`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/8545_Zadanie próbne.java
@@ -0,0 +1,23 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        List<String> reversedEachAlphabet = Arrays.stream(br.readLine().split("")).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        for (String str : reversedEachAlphabet) {
+            bw.write(str);
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
