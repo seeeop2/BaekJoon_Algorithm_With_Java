2579de832c5890489c04aa6587271026dcf87b1f
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-02-24 00:25:37 +0900

    Finished `#10171_고양이`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/10171_고양이.java
@@ -0,0 +1,19 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String cat = "\\    /\\\n" +
+                    " )  ( ')\n" +
+                    "(  /  )\n" +
+                    " \\(__)|";
+
+        bw.write(cat);
+
+        bw.flush();
+        bw.close();
+    }
+}
