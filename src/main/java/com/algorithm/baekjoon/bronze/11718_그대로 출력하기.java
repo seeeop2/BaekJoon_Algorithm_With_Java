f1b8a99693340a20ca904022f2cb541caf6228b8
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-09 02:27:25 +0900

    Finished `#11718_그대로 출력하기`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/11718_그대로 출력하기.java
@@ -0,0 +1,21 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String input;
+
+        while ( (input = br.readLine()) != null ){
+            input = input.trim();
+            bw.write(input);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
