decd1457c2493c846664f19cadb0551d4d4f2110
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-12 00:34:27 +0900

    Finished `#15000_CAPS`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/15000_CAPS.java
@@ -0,0 +1,16 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String input = br.readLine().toUpperCase();
+        bw.write(input);
+
+        bw.flush();
+        bw.close();
+    }
+}
