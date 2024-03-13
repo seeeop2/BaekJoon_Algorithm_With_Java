af97be4d5d70bb3c016d197e6bec605ef0cb9c17
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-13 16:19:18 +0900

    Finished `#15733_나는 누구인가`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/15733_나는 누구인가.java
@@ -0,0 +1,16 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String output = "I'm Sexy";
+        
+        bw.write(output);
+        
+        bw.flush();
+        bw.close();
+    }
+}
