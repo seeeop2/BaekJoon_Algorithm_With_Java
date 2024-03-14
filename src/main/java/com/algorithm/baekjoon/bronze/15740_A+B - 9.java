741e93ea308b513731691395ec293eca381be5e3
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-14 17:23:45 +0900

    Finished `#15740_A+B - 9`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/15740_A+B - 9.java
@@ -0,0 +1,26 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        List<BigInteger> list = Arrays.stream(br.readLine().split(" ")).map(it -> new BigInteger(it)).collect(Collectors.toList());
+
+        BigInteger first = list.get(0);
+        BigInteger second = list.get(1);
+
+        BigInteger addition = first.add(second);
+
+        bw.write(addition.toString());
+
+        bw.flush();
+        bw.close();
+    }
+}
