b75b3a9d00f57d093f5771c844e2f65a90506a80
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-14 17:24:38 +0900

    Finished `#15962_새로운 시작`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/15962_새로운 시작.java
@@ -0,0 +1,16 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String message = "파이팅!!";
+
+        bw.write(message);
+
+        bw.flush();
+        bw.close();
+    }
+}
