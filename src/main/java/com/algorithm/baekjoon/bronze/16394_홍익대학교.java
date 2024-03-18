14cc48bde8029a8d91931156cf232d661b1e636f
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-18 12:50:31 +0900

    Finished `#16394_홍익대학교`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/16394_홍익대학교.java
@@ -0,0 +1,15 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int openingOfHongik = 1946;
+        int input = scanner.nextInt();
+        int gap = input - openingOfHongik;
+
+        System.out.println(gap);
+    }
+}
