22e52f04b7a191e25781a55d54fc2c18be885336
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-19 16:59:31 +0900

    Finished `#18096_Арифметическая магия`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/18096_Арифметическая магия.java
@@ -0,0 +1,17 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int N = Integer.parseInt(br.readLine());
+        Integer result = (int) Math.pow(1, N);
+
+        bw.write(result.toString());
+        bw.flush();
+        bw.close();
+    }
+}
