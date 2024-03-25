0b669226a089230d8fce06df88c7efe94c5f8736
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-25 11:29:49 +0900

    Finished `#24078_余り (Remainder)`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/24078_余り (Remainder).java
@@ -0,0 +1,18 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int number = 21;
+        int x = Integer.parseInt(br.readLine());
+        int remainder = x % number;
+
+        bw.write(String.valueOf(remainder));
+        bw.flush();
+        bw.close();
+    }
+}
