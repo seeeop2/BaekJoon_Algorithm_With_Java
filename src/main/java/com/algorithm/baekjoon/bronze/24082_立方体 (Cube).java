cc2a05f2872d0c447dcec28ed9081bff7f528c96
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-25 12:53:29 +0900

    Finished `#24082_立方体 (Cube)`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/24082_立方体 (Cube).java
@@ -0,0 +1,17 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int x = Integer.parseInt(br.readLine());
+        int volume = (int) Math.pow(x, 3);
+
+        bw.write(String.valueOf(volume));
+        bw.flush();
+        bw.close();
+    }
+}
