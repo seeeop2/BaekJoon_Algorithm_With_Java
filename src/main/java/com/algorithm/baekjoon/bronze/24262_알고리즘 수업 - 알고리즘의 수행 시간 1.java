3b33ccd028d3afa0657092f16540782bc76e6b5e
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-26 14:22:56 +0900

    Finished `#24262_알고리즘 수업 - 알고리즘의 수행 시간 1`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/24262_알고리즘 수업 - 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        bw.write("1\n");
+        bw.write("0");
+
+        bw.flush();
+        bw.close();
+    }
+}
