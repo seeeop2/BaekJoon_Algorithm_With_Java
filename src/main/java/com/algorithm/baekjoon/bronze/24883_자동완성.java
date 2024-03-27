7d54a27850dd9732335d1aba1007e9a7477e9f48
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-03-28 01:18:05 +0900

    Finished `#24883_자동완성`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/24883_자동완성.java
@@ -0,0 +1,21 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        String alphabet = br.readLine().toUpperCase();
+
+        if (alphabet.equals("N")){
+            bw.write("Naver D2");
+        } else{
+            bw.write("Naver Whale");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
