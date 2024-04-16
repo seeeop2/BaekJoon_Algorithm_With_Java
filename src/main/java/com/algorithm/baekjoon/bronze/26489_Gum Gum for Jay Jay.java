856f215c4ba3abd8e3d93d435c76e34add33d536
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-04-16 21:12:16 +0900

    Finished `#26489_Gum Gum for Jay Jay`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/26489_Gum Gum for Jay Jay.java
@@ -0,0 +1,28 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String sentence = "gum gum for jay jay";
+        String input;
+        int count = 0;
+
+        while ((input = br.readLine()) != null){
+            if(input.equals(sentence)){
+                count++;
+            }
+        }
+        
+        bw.write(String.valueOf(count));
+
+        bw.flush();
+        bw.close();
+    }
+}
