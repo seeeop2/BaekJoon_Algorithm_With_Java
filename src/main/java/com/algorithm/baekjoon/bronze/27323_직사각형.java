1edc86d6a7cb98521393f721134646530dba9e29
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-04-22 23:12:34 +0900

    Finished `#27323_직사각형`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/27323_직사각형.java
@@ -0,0 +1,20 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int height = Integer.parseInt(br.readLine());
+        int width = Integer.parseInt(br.readLine());
+
+        int area = height * width;
+
+        bw.write(String.valueOf(area));
+
+        bw.flush();
+        bw.close();
+    }
+}
