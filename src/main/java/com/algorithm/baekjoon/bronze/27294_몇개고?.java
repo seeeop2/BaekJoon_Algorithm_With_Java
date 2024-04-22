0c4fa66321f6b2c1a21ecf1e93c691e3c634830f
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-04-22 23:11:39 +0900

    Finished `#27294_몇개고?`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/27294_몇개고?.java
@@ -0,0 +1,41 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int drinkWithSushiOrNotLunch = 280;
+        int noDrinkWithSushiDuringLunch = 320;
+        String mealTime = "";
+
+        List<Integer> inputList = Arrays.stream(br.readLine().split(" ")).map(it -> Integer.parseInt(it)).collect(Collectors.toList());
+        int time = inputList.get(0);
+        int drinkOrNot = inputList.get(1);
+
+        if(time <= 11){
+            mealTime = "Breakfast";
+        }
+        else if(12 <= time && time <= 16){
+            mealTime = "Lunch";
+        }
+        else if(17 <= time && time <=23){
+            mealTime = "Dinner";
+        }
+
+        if (mealTime == "Lunch" && drinkOrNot == 0){
+            bw.write(String.valueOf(noDrinkWithSushiDuringLunch));
+        }
+        else{
+            bw.write(String.valueOf(drinkWithSushiOrNotLunch));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
