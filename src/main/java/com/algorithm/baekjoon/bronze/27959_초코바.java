7d9e288e4e70be5a4be1e9560b86995f13a1ead8
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-05-03 01:18:41 +0900

    Finished `#27959_초코바`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/27959_초코바.java
@@ -0,0 +1,29 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> inputList = Arrays.stream(br.readLine().split(" ")).map(it -> Integer.parseInt(it)).collect(Collectors.toList());
+
+        int coin = 100;
+        int numberOfCoin = inputList.get(0);
+        int priceOfCandyBar = inputList.get(1);
+
+        if (coin * numberOfCoin >= priceOfCandyBar){
+            bw.write("Yes");
+        }
+        else{
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
