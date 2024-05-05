53250b3eecc82933e215dac8c4855346fce9c31b
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-05-05 23:27:55 +0900

    Finished `#28927_Киноманы`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/28927_Киноманы.java
@@ -0,0 +1,42 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int runningTimeOfTrailer = 3;
+        int runningTimeOfSeries = 20;
+        int runningTimeOfMovie = 120;
+
+        List<Integer> informationOfMax = Arrays.stream(br.readLine().split(" ")).map(it -> Integer.parseInt(it)).collect(Collectors.toList());
+        List<Integer> informationOfMel = Arrays.stream(br.readLine().split(" ")).map(it -> Integer.parseInt(it)).collect(Collectors.toList());
+
+        int totalTimeOfMax =
+                        runningTimeOfTrailer * informationOfMax.get(0) +
+                        runningTimeOfSeries * informationOfMax.get(1) +
+                        runningTimeOfMovie * informationOfMax.get(2);
+        int totalTimeOfMel =
+                        runningTimeOfTrailer * informationOfMel.get(0) +
+                        runningTimeOfSeries * informationOfMel.get(1) +
+                        runningTimeOfMovie * informationOfMel.get(2);
+
+        if (totalTimeOfMax > totalTimeOfMel){
+            bw.write("Max");
+        }
+        else if( totalTimeOfMel > totalTimeOfMax){
+            bw.write("Mel");
+        }
+        else{
+            bw.write("Draw");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
