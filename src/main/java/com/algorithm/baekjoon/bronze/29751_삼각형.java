45b080a3ead4b93fafc245f2bbcb92c24a85839d
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-05-08 20:31:37 +0900

    Finished `#29751_삼각형`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/29751_삼각형.java
@@ -0,0 +1,25 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> inputList = Arrays.stream(br.readLine().split(" ")).map(it -> Integer.parseInt(it)).collect(Collectors.toList());
+        int w = inputList.get(0);
+        int h = inputList.get(1);
+
+        double area = w * h * 0.5;
+        String areaToString = String.format("%.1f", area);
+
+        bw.write(areaToString);
+
+        bw.flush();
+        bw.close();
+    }
+}
