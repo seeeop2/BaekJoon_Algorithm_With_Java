6614f0b37eedec5d05f9c85afb5e6100cc8c0d33
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-05-16 01:35:10 +0900

    Finished `#30664_Loteria Falha`

---
+++ src/main/java/com/algorithm/baekjoon/bronze/30664_Loteria Falha.java
@@ -0,0 +1,27 @@
+package com.algorithm.baekjoon.bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        BigInteger input;
+        int luckyNumber = 42;
+
+        while(!(input = new BigInteger(br.readLine())).equals(BigInteger.valueOf(0))){
+            if (input.mod(BigInteger.valueOf(luckyNumber)).equals(BigInteger.valueOf(0))){
+                bw.write("PREMIADO");
+            }
+            else{
+                bw.write("TENTE NOVAMENTE");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
