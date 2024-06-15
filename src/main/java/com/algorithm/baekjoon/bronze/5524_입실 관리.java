0fc57186d6bb1ffce6d56d3c736ff2a976cb3556
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-06-16 00:48:21 +0900

    Create 5524_입실 관리.java

---
+++ src/main/java/com/algorithm/baekjoon/bronze/5524_입실 관리.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < n ; i++) {
+            String input = br.readLine().toLowerCase();
+            
+            bw.write(input);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
