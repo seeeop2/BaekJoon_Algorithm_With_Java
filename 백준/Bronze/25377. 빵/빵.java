cee52595c828f2d8530f7f8c1e225cb7fef6335e
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2024-11-09 22:23:00 +0900

    [Bronze IV] Title: 빵, Time: 104 ms, Memory: 14312 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int minimum = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            if (a <= b) {
+                minimum = Math.min(minimum, a);
+            }
+        }
+
+        if (minimum == Integer.MAX_VALUE) {
+            bw.write("-1");
+        } else {
+            bw.write(String.valueOf(minimum));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
