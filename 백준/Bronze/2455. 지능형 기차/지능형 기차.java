7b00e72efbca95094a7bfa3bdd7a00039a7a85f1
seeeop2 <69149757+seeeop2@users.noreply.github.com>
2025-02-14 12:04:41 +0900

    [Bronze III] Title: 지능형 기차, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    // 시간 복잡도: O(1)
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        final int NUMBER_OF_STATIONS = 4;
+        int currentPeople = 0;
+        int maxPeople = 0;
+
+        for (int i = 0; i < NUMBER_OF_STATIONS; i++) {
+            String[] s = br.readLine().split(" ");
+            int peopleOut = Integer.parseInt(s[0]);
+            int peopleIn = Integer.parseInt(s[1]);
+            currentPeople -= peopleOut;
+            currentPeople += peopleIn;
+            maxPeople = Math.max(maxPeople, currentPeople);
+        }
+
+        bw.write(Integer.toString(maxPeople));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
